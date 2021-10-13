6d950675e56bb6020d28546d6f6a997347ef9755
sdh0611 <tlaeogus0611@naver.com>
2021-10-13 22:23:03 +0900

    20211013|Custom skeletal mesh component.

---
+++ Source/ProjectDSFramework/Public/Character/DSSkeletalMeshComponent.h
@@ -0,0 +1,17 @@
+// SDH, All rights reserved. (2021 ~ )
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "DSSkeletalMeshComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDSFRAMEWORK_API UDSSkeletalMeshComponent : public USkeletalMeshComponent
+{
+	GENERATED_BODY()
+	
+};
